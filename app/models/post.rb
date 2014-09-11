7a3ab89d94bebf18b0c22d6741524fe12e8bc261
Mackenzie Child <mackenzie.child@gmail.com>
2014-09-11 17:04:27 -0600

    Add pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
+  get '/about', to: 'pages#about'
+
 end
