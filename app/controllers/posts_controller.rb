12e6e63d1c710c5dfabbff9f67a427d534955dbb
Mackenzie Child <mackenzie.child@gmail.com>
2014-09-11 14:42:54 -0600

    Finish Posts and styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def show
@@ -13,8 +14,32 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(post_params)
 
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 	private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true, length: {minimum: 5}
+	validates :body,  presence: true
 end
